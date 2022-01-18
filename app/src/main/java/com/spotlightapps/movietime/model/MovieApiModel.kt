7ec8f8992e27e001c8cba35bd6c95af1a6df42df
Ahmad Jawid Muhammadi <spotlightapps18@gmail.com>
2022-01-18 22:13:19 +0530

    Added movie api model

---
+++ app/src/main/java/com/spotlightapps/movietime/model/MovieApiModel.kt
@@ -0,0 +1,46 @@
+package com.spotlightapps.movietime.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieApiModel(
+    @SerializedName("page")
+    var page: Int?,
+    @SerializedName("results")
+    var results: List<Result>?,
+    @SerializedName("total_pages")
+    var totalPages: Int?,
+    @SerializedName("total_results")
+    var totalResults: Int?
+)
+
+data class Result(
+    @SerializedName("adult")
+    var adult: Boolean?,
+    @SerializedName("backdrop_path")
+    var backdropPath: String?,
+    @SerializedName("genre_ids")
+    var genreIds: List<Int>?,
+    @SerializedName("id")
+    var id: Int?,
+    @SerializedName("original_language")
+    var originalLanguage: String?,
+    @SerializedName("original_title")
+    var originalTitle: String?,
+    @SerializedName("overview")
+    var overview: String?,
+    @SerializedName("popularity")
+    var popularity: Double?,
+    @SerializedName("poster_path")
+    var posterPath: String?,
+    @SerializedName("release_date")
+    var releaseDate: String?,
+    @SerializedName("title")
+    var title: String?,
+    @SerializedName("video")
+    var video: Boolean?,
+    @SerializedName("vote_average")
+    var voteAverage: Double?,
+    @SerializedName("vote_count")
+    var voteCount: Int?
+)
