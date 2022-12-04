ed0a941c5c2b0e919916e7e967df7b0a3c33975c
Gitlab CI <gitlab-ci@ocado.com>
2022-12-04 21:29:28 +0100

    convert build.gradle to kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext.hilt_version = "2.44.2"
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath "com.android.tools.build:gradle:7.0.4"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0"
-        classpath "com.google.dagger:hilt-android-gradle-plugin:$hilt_version"
+        classpath ("com.android.tools.build:gradle:7.0.4")
+        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.44.2")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
